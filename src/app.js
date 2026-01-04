8a141b780d45e15db5d780ef89063e43157bd4a8
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-04 16:56:27 +0530

    Playing with request handlers

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Harshit", lastName: "Raj" });
-});
-
-app.post("/user", (req, res) => {
-  console.log(res.body);
-  // saving data to DB
-  res.send("Data successfully saved to database!!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// This will match all the HTTP methods API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the serverr!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling request user 1!");
+    next();
+    res.send("1st response");
+  },
+  (req, res, next) => {
+    console.log("Handling request user 2!");
+    next();
+    // res.send("2nd response");
+  },
+  (req, res, next) => {
+    console.log("Handling request user 3!");
+    // next();
+    // res.send("3rd response");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is serving at port 7777");
