2b8252d469fc1e7b88f91cc795b2abb0edc49d68
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-04 21:01:48 +0530

    Created User model and ading data to DB using /signup and Postman

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const User = require("./models/user.model.js");
 
 const app = express();
 
 const connectDB = require("./config/database.js");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Priya",
+    lastName: "Saha",
+    email: "priya@gmail.com",
+    password: "Priya@9",
+  });
+  try {
+    user.save();
+    res.send("User saved successfully!!!");
+  } catch (error) {
+    res.status(400).send("Error saving user : " + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
